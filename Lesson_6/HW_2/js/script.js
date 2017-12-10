257c651f9759546e9d818506b2f09c012dfbb0d8
Viktor <admin@rizne.in.ua>
2017-12-11 01:55:24 +0200

    11-12-2017

--- Lesson_6/HW_2/js/script.js
+++ Lesson_6/HW_2/js/script.js
@@ -2,17 +2,30 @@ document.onkeyup = function() {
     var keyCode = window.event ? window.event.keyCode : event.which;
     action(keyCode);
 }
+// function action(keyCode) {
+//     var pushkey = event.keyCode;
+//     if (pushkey == 17 && 114 ) { 
+//         alert('Виділено ВСЕ');
+//     }
+//     if (pushkey == 17 && 83 ) { 
+//         alert('Збережено');
+//     }
+//     if (pushkey == 16 && 17 && 83 ) { 
+//         alert('збережено ВСЕ');
+//     }
+// }
+
+// Потім попробую через кейси зробити
 function action(keyCode) {
     var pushkey = event.keyCode;
     if (pushkey == 17 && 114 ) { 
         alert('Виділено ВСЕ');
     }
-    if (pushkey == 17 && 83 ) { 
+    else if (pushkey == 17 && 83 ) { 
         alert('Збережено');
     }
-    if (pushkey == 16 && 17 && 83 ) { 
+    else if (pushkey == 16 && 17 && 83 ) { 
         alert('збережено ВСЕ');
     }
+    else {}
 }
-
-// Потім попробую через кейси зробити
